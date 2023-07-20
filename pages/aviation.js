97fd30e4c472fb0e3b94c719ed4ecb96ba2be85f
Dean Opara <deanopara48@gmail.com>
2023-07-20 16:49:27 +0200

    renew aviation page

--- pages/aviation.js
+++ pages/aviation.js
@@ -1,14 +1,35 @@
 
+  //Components
   import React from 'react'
-  import '../styles/default.css'
   import NavigationBar from '../components/NavigationBar'
+  import SideBar from "../components/SideBar";
+  import SideLink from "../components/SideLink";
+  import Card from "../components/card"
+  import Banner from "../components/banner"
+  //CSS
+  import '../styles/default.css'
+  import '../styles/sideBar.css'
+  //Images
+  import aviation1 from "../media/aviation1.jpg"
+  
+  const links =[
+    new SideLink("displayName", "#id", "issubpoint")
+  ]
   
   export default function Aviation() {
     return (
       <>
-      <NavigationBar></NavigationBar>
-      <h1 className='center headline'>
-        aviation</h1>
+        <NavigationBar></NavigationBar>
+        <div className="grid">
+          <Banner
+             title={"aviation"}
+             img={aviation1}>
+          </Banner>
+          <div>
+             <SideBar links={links} />
+          </div>
+          
+        </div>
       </>
     )
   }
