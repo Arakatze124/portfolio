8eb3174f80ab7e608870cfe33c883f14eb831bc3
Dean Opara <deanopara48@gmail.com>
2023-07-21 10:45:12 +0200

    Rechtschreibfehler

--- pages/home.js
+++ pages/home.js
@@ -12,12 +12,12 @@ export default function Home() {
   return (
     <>
     <NavigationBar></NavigationBar>
-    <h1 className='center headline'>
+    <h1 className='center title2'>
       Home</h1>
       <Card 
         img={Home1}
         text={
-          <p className=''><b>Herzlich Willkommen auf meiner Website!</b><br></br><br></br> Ich habe diese Website auf eigene Hand erstellt und Sie dient dazu, meine Wenigkeit der Öffentlichkeit darzustellen. Hier erfüährt man, was ich in meiner Freizeit mache und wie meine Karriere bisher verlaufen ist. <br></br><br></br>Falls das Interesse besteht Kontakt aufzunehmen, gibt es <Link href={'/contact'}>hier</Link> die Möglickeit.</p>}
+          <p className=''><b>Herzlich Willkommen auf meiner Website!</b><br></br><br></br> Ich habe diese Website auf eigene Hand erstellt und Sie dient dazu, meine Wenigkeit der Öffentlichkeit darzustellen. Hier erfüährt man, was ich in meiner Freizeit mache und wie meine Karriere bisher verlaufen ist. <br></br><br></br>Falls das Interesse besteht Kontakt aufzunehmen, gibt es <Link href={'/contact'}>hier</Link> die Möglichkeit.</p>}
         headline={"Vorwort"}
         position={"left"}>
       </Card>
