3df4f73d3ec016d5f88aef6d6afa92f8a0deff97
Dean Opara <deanopara48@gmail.com>
2025-02-24 13:30:17 +0100

    finish banner animations

--- pages/newIndex.js
+++ pages/newIndex.js
@@ -2,8 +2,9 @@ import "../styles/default.css"
 import Layout from "../components/layout"
 import Banner from "../components/banner"
 import Home1 from "../media/home2.jpg"
-import { useRef, useEffect, useState, use } from "react"
+import { useRef, useEffect, useState } from "react"
 import { gsap } from "gsap"
+
 export default function Home() {
     const [textIndex, setTextIndex] = useState(0)
     const titleRef = useRef(null)
@@ -16,14 +17,14 @@ export default function Home() {
         "Student",
         "Musician",
         "Biker",
-        "Gamer",
+    
     ]
 
     const startCarousel = () => {
         const tl = gsap.timeline({ repeat: -1, repeatDelay: 0 });
-
-        tl.fromTo(subtitleRef.current,{opacity:0, x:-100}, { x: 0, opacity: 1, duration: 1, ease: "" }) // Step 2: Slide in subtitle
-          .to(subtitleRef.current, { x: 100, opacity: 0, duration: 1, delay: 3, ease: "" }) // Step 4: Slide out subtitle
+        
+        tl.fromTo(subtitleRef.current, { opacity: 0, x: -100 }, { x: 0, opacity: 1, duration: 1, ease: "sine" }) // Step 2: Slide in subtitle
+          .to(subtitleRef.current, { x: 100, opacity: 0, duration: 1, delay: 3, ease: "sine" }) // Step 4: Slide out subtitle
           .call(() => {
               // Step 5: Change text
               setTextIndex((prevIndex) => (prevIndex + 1) % subtitles.length);
@@ -31,27 +32,10 @@ export default function Home() {
     };
    
     useEffect(() => {
-       gsap.fromTo(titleRef.current, {opacity: 0}, {opacity: 1, duration: 1, onComplete: () => {startCarousel()}})
-       //1 wait until title is visible
-       //2 slide in subtitle
+        gsap.fromTo(subtitleRef.current, { opacity: 0 }, { opacity: 1, duration: 1, delay:1 });
+       gsap.fromTo(titleRef.current, { opacity: 0 }, { opacity: 1, duration: 1, onComplete: () => {
+        startCarousel()} });
     }, [])
-
-    useEffect(() => {
-        //3 wait 3 seconds
-        //4 slide out subtitle
-        //5 change text
-        //6 slide in subtitle
-        //7 repeat from 3
-        
-        
-    }, [])
-
-    useEffect(() => {
-        if(counter % 1 == 0){
-        setTextIndex(counter % subtitles.length)
-    }
-        console.log(counter)
-    }, [counter])
 
     return(
         <div>
